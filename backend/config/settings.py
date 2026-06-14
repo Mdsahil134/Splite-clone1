bf666b52a7a4204767513d9d5dcb9dc8f20f48d1
Md sahil <smd859796@gmail.com>
2026-06-15 00:43:52 +0530

    add git ignore

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,6 +131,6 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
     "http://localhost:5173,http://127.0.0.1:5173",
 ).split(",")
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
